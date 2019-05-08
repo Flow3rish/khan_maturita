b92a96a2e4f1c9359744636bba9fd929b67af9a2
Vít Chrubasík <flow3rish@protonmail.com>
2019-05-08 20:01:14 +0200

    úprava struktury

--- auto/seminarni_prace.el
+++ auto/seminarni_prace.el
@@ -5,12 +5,12 @@
                      '(("report" "12pt" "a4paper")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("babel" "czech" "") ("ulem" "normalem") ("hyperref" "hidelinks") ("biblatex" "backend=bibtex" "citestyle=authoryear")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -38,13 +38,15 @@
     "biblatex"
     "tikz")
    (LaTeX-add-labels
-    "sec:orgc278236"
-    "sec:org98b5037"
-    "sec:org754ac0a"
-    "sec:orgd71be65"
-    "sec:org4a8c6a8"
-    "sec:orge54880c"
-    "sec:orgb9893ea")
+    "sec:org363e3db"
+    "sec:orgb803dc7"
+    "sec:org08a7d98"
+    "sec:orgb9e6b0d"
+    "sec:org551b973"
+    "sec:org7efa980"
+    "sec:org3db359f"
+    "sec:orgf5bae8a"
+    "sec:org338994c")
    (LaTeX-add-bibliographies
     "~/OneDrive/Orgmode/Papers/references"))
  :latex)
